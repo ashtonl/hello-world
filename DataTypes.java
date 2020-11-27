d91f474940ed679453db13ef75021ef064210b95
ashton <123646478@qq.com>
2020-11-27 16:29:30 +0800

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,22 @@
+/*
+Basic Data Types:
+byte, short, int, long;
+float, double;
+boolean;
+char.
+
+*/
+
+public class DataTypes {
+    public static void main(String[] args) {
+        char c = 'a';
+        System.out.println(c);
+        System.out.print('\n');
+        System.out.println("\n\"Hello, World!\"");
+
+        long y = 2147483648L;
+        System.out.println(y);
+    }
+
+}
+
